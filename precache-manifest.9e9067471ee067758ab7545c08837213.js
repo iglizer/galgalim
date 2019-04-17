434b469a0bcfb65da48a4ee4f5965c65c72e0b0f
Igor Glizer <igor@rookout.com>
2019-04-17 23:41:28 +0300

    Updates

---
+++ precache-manifest.9e9067471ee067758ab7545c08837213.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "9bf0e8ddb8fc95c99b6b",
+    "url": "/galgalim/static/js/runtime~main.f148a28d.js"
+  },
+  {
+    "revision": "fcb2d04a013b892cd417",
+    "url": "/galgalim/static/js/main.7ee59b78.chunk.js"
+  },
+  {
+    "revision": "be403ea534b56a9bc09a",
+    "url": "/galgalim/static/js/2.5a585bf8.chunk.js"
+  },
+  {
+    "revision": "76d84ef1c7055ab31a9076072f7277ba",
+    "url": "/galgalim/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.16bc319dcb99a58be0f566a17c17957a.js"
+  "/galgalim/precache-manifest.9e9067471ee067758ab7545c08837213.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/galgalim/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
